83206b725e06929301391fc1b0706a834e893002
unknown <afkhamis1386@gmail.com>
2026-06-26 00:04:00 +0330

    feat: create NormalUser class with features requested in documentation

--- BookCLUB/Common/Normaluser.h
+++ BookCLUB/Common/Normaluser.h
@@ -1,4 +1,38 @@
 #ifndef NORMALUSER_H
 #define NORMALUSER_H
-
+#include "user.h"
+#include <QString>
+#include <QVector>
+#include <QMap>
+class NormalUser : public User {
+    Q_OBJECT
+private:
+    QVector<QString> favoriteGenres;
+    QVector<int> purchasedBooksIds;
+    QVector<int> savedBooksIds;
+    double walletBalance;
+    QMap<int, int> lastReadPages;
+public:
+    NormalUser();
+    NormalUser(QString us, QString pwh, QString sq, QString hsqa, bool ib, double balance = 0.0);
+    ~NormalUser() ;
+    QString getRole() const;
+    bool setFavoriteGenres(const QVector<QString> &genres);
+    QVector<QString> getFavoriteGenres() const;
+    void addPurchasedBook(int bookId);
+    int getPurchasedCount() const;
+    QVector<int> getPurchaseHistory() const;
+    void saveBook(int bookId);
+    void removeSavedBook(int bookId);
+    QVector<int> getSavedBooks() const;
+    double getWalletBalance() const;
+    bool deductFromWallet(double amount);
+    void addToWallet(double amount);
+    bool addReview(int bookId, QString comment, int rating);
+    bool editReview(int reviewId, QString newComment);
+    bool deleteReview(int reviewId);
+    bool readBook(int bookId);
+    void saveLastReadPage(int bookId, int pageNumber);
+    int getLastReadPage(int bookId) const;
+};
 #endif // NORMALUSER_H
