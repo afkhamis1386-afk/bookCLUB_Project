46cc9184a09d0ad1ef3289b856b4feef3788ede2
unknown <afkhamis1386@gmail.com>
2026-06-30 16:04:56 +0330

    refactor: align NormalUser implementation with base User class and fix file casing

---
+++ common/normaluser.cpp
@@ -0,0 +1,82 @@
+#include "normaluser.h"
+NormalUser::NormalUser():User(){}
+NormalUser::NormalUser(const QString &username, const QString &plainPassword, const QString &plainAnswer)
+    :User(username, plainPassword, plainAnswer){}
+NormalUser::NormalUser(int userId, const QString &encryptedUsername, const QString &passwordHash, const QString &answerHash, bool isBlocked, const QDateTime &registerDate)
+    :User(userId, encryptedUsername, passwordHash, answerHash, isBlocked, registerDate){}
+NormalUser::~NormalUser(){}
+QString NormalUser::getRole() const {
+    return "NormalUser";
+}
+bool NormalUser::setFavoriteGenres(const QVector<int> &genreIds){
+    if(genreIds.size() < 1 || genreIds.size() > 3)
+        return false;
+    QVector<int> unique;
+    for(int id : genreIds){
+        if(!unique.contains(id))
+            unique.append(id);
+    }
+    if(unique.size() < 1 || unique.size() > 3)
+        return false;
+    favoriteGenreIds = unique;
+    return true;
+}
+QVector<int> NormalUser::getFavoriteGenres() const {
+    return favoriteGenreIds;
+}
+void NormalUser::setPurchasedBooks(const QVector<int> &bookIds){
+    QVector<int> unique;
+    for(int id : bookIds){
+        if(!unique.contains(id))
+            unique.append(id);
+    }
+    purchasedBookIds = unique;
+}
+void NormalUser::addPurchasedBook(int bookId){
+    if(!purchasedBookIds.contains(bookId))
+        purchasedBookIds.append(bookId);
+}
+bool NormalUser::hasPurchased(int bookId) const {
+    return purchasedBookIds.contains(bookId);
+}
+int NormalUser::getPurchasedCount() const {
+    return purchasedBookIds.size();
+}
+QVector<int> NormalUser::getPurchaseHistory() const {
+    return purchasedBookIds;
+}
+void NormalUser::setSavedBooks(const QVector<int> &bookIds){
+    QVector<int> unique;
+    for(int id : bookIds){
+        if(!unique.contains(id))
+            unique.append(id);
+    }
+    savedBookIds = unique;
+}
+void NormalUser::saveBook(int bookId){
+    if(!savedBookIds.contains(bookId))
+        savedBookIds.append(bookId);
+}
+void NormalUser::removeSavedBook(int bookId){
+    savedBookIds.removeAll(bookId);
+}
+bool NormalUser::isBookSaved(int bookId) const {
+    return savedBookIds.contains(bookId);
+}
+QVector<int> NormalUser::getSavedBooks() const {
+    return savedBookIds;
+}
+void NormalUser::setReadingProgress(const QMap<int, int> &progress){
+    lastReadPages.clear();
+    for(auto it = progress.constBegin(); it != progress.constEnd(); ++it){
+        if(it.value() >= 0)
+            lastReadPages[it.key()] = it.value();
+    }
+}
+void NormalUser::setLastReadPage(int bookId, int pageNumber){
+    if(pageNumber >= 0)
+        lastReadPages[bookId] = pageNumber;
+}
+int NormalUser::getLastReadPage(int bookId) const {
+    return lastReadPages.value(bookId, 0);
+}
