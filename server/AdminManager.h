1ad2f7a65985b7e60ad77cbf162642289ec7a088
unknown <afkhamis1386@gmail.com>
2026-07-06 12:24:45 +0330

    feat: implement AdminManager class to server

---
+++ server/AdminManager.h
@@ -0,0 +1,20 @@
+#ifndef ADMINMANAGER_H
+#define ADMINMANAGER_H
+#include "../common/Response.h"
+#include <QString>
+class AdminManager {
+public:
+    AdminManager();
+    Response getAllUsers();
+    Response getNormalUserDetails(int userId);
+    Response getPublisherDetails(int userId);
+    Response blockUser(int userId);
+    Response unblockUser(int userId);
+    Response deleteUser(int userId);
+    Response getAllBooks();
+    Response getBookDetailsForReview(int bookId);
+    Response removeInvalidBook(int bookId);
+    Response getAllReviews();
+    Response removeInappropriateReview(int reviewId);
+};
+#endif // ADMINMANAGER_H
