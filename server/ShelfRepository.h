4d1a2fe2065cd5feb9aece774085aca0af78e078
unknown <afkhamis1386@gmail.com>
2026-07-28 01:57:34 +0330

    edit the shelfrepository

--- server/ShelfRepository.h
+++ server/ShelfRepository.h
@@ -1,18 +1,23 @@
 #ifndef SHELFREPOSITORY_H
 #define SHELFREPOSITORY_H
+
 #include "../common/Shelf.h"
 #include <QString>
 #include <QVector>
+
 class ShelfRepository {
 public:
     ShelfRepository();
     int insertShelf(const Shelf &shelf);
     Shelf* loadShelfById(int shelfId);
     QVector<int> getShelfIdsByUser(int userId);
+    QVector<int> getBookIdsByShelf(int shelfId);
     bool updateShelfName(int shelfId, const QString &newName);
     bool deleteShelf(int shelfId);
     bool addBookToShelf(int shelfId, int bookId);
     bool removeBookFromShelf(int shelfId, int bookId);
+    bool reorderShelves(int userId, const QVector<int> &shelfIds);
+    bool reorderShelfBooks(int shelfId, const QVector<int> &bookIds);
     bool shelfNameExistsForUser(int userId, const QString &shelfName);
     bool shelfBelongsToUser(int shelfId, int userId);
 };
