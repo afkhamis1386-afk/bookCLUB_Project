43b3bd9ee579617ff66af52ee7d40395c561a033
mahshid qotbzadeh <mahshid1384ghotbzd@gmail.com>
2026-07-27 23:29:18 +0330

    feat(client-handler): route UpdateAccount requests with role validation

--- server/ClientHandler.cpp
+++ server/ClientHandler.cpp
@@ -120,6 +120,12 @@ void ClientHandler::processRequest(const Request &req) {
         case RequestType::GetAccountInfo:
             response = handleAuthRequest(req);
             break;
+        case RequestType::UpdateAccount:
+            if (checkRole({UserRole::NormalUser, UserRole::Publisher}, accessError))
+                response = handleAuthRequest(req);
+            else
+                response = accessError;
+            break;
         case RequestType::GetAllGenres:
         case RequestType::GetAllCategories:
             response = handleAuthRequest(req);
@@ -308,6 +314,8 @@ Response ClientHandler::handleAuthRequest(const Request &req) {
         return authManager.changePassword(authenticatedUserId, authenticatedRole, p.value("oldPassword").toString(), p.value("newPassword").toString());
     case RequestType::GetAccountInfo:
         return authManager.getAccountInfo(authenticatedUserId, authenticatedRole);
+    case RequestType::UpdateAccount:
+        return authManager.updateAccount(authenticatedUserId, authenticatedRole, p);
     case RequestType::RecoverPassword:
         return authManager.recoverPassword(p.value("username").toString(), p.value("securityAnswer").toString(), p.value("newPassword").toString());
     case RequestType::GetAllGenres:
