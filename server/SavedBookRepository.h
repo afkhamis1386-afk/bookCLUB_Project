0348959f488185dd5e4a1edc3f0201a11c0f002c
mahshid qotbzadeh <mahshid1384ghotbzd@gmail.com>
2026-07-28 06:43:59 +0330

    feat(saved-books): add favorite books repository support and ordering

--- server/SavedBookRepository.h
+++ server/SavedBookRepository.h
@@ -2,6 +2,7 @@
 #define SAVEDBOOKREPOSITORY_H
 
 #include <QVector>
+
 class SavedBookRepository {
 public:
     SavedBookRepository();
@@ -10,6 +11,11 @@ public:
     bool unsaveBook(int userId, int bookId);
     QVector<int> getSavedBookIds(int userId);
     QVector<int> getUserIdsWhoSavedBook(int bookId);
+    bool isFavoriteBook(int userId, int bookId);
+    bool addFavoriteBook(int userId, int bookId);
+    bool removeFavoriteBook(int userId, int bookId);
+    QVector<int> getFavoriteBookIds(int userId);
+    bool reorderFavoriteBooks(int userId, const QVector<int> &bookIds);
 };
 
 #endif // SAVEDBOOKREPOSITORY_H
